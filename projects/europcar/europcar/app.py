d6af81fe04430f29d15c7a957d25bc9fdc317115
mbaryshnikov <mbaryshnikov78@gmail.com>
2022-10-27 13:05:12 +0200

    europcar

--- projects/europcar/europcar/app.py
+++ projects/europcar/europcar/app.py
@@ -19,7 +19,8 @@ def db_exec(f, st):
 
 
 def cars_print(stat):
-    sql = f"SELECT Cars.ID, m.Producer, m.Model, m.Class, m.Price FROM Cars JOIN Colors as 'c' on c.ID = Cars.Color JOIN Models as 'm' on m.ID = Cars.Model JOIN Status as 's' on s.ID = Cars.Status WHERE Avl = {stat} ORDER BY m.Producer;"
+    sql = f"SELECT Cars.ID, m.Producer, m.Model, m.Class, m.Price FROM Cars JOIN Colors as 'c' on c.ID = Cars.Color\
+        JOIN Models as 'm' on m.ID = Cars.Model JOIN Status as 's' on s.ID = Cars.Status WHERE Avl = {stat} ORDER BY m.Producer;"
     lst = db_exec('europcar.db', sql)
     columns = ('ID', 'Produser', 'Model', 'Class', 'Price')
     print('-' * 38)
@@ -33,15 +34,15 @@ def main():
     print('-' * 38)
     print('Welcome by Europcar!')
     print('avaiable cars:')
-    cars_list = cars_print(1)
+    cars_list = cars_print(1) # 1 - availabl
     user_ch = int(input('User_Wish ID: '))
-    user_ch = [x for x in cars_list if x[0] == user_ch]
+    user_ch = [x for x in cars_list if x[0] == user_ch][0]
     if user_ch == []:
         print('There is no such car!')
     else:
         user_days = int(input('For how many days: '))
-        print(f'Your rent will be: {float(user_ch[0][4]) * int(user_days):.2f}$ ')
-        sql = f"UPDATE Cars SET Status = 2 WHERE ID = {user_ch[0][0]};"
+        print(f'Your rent will be: {float(user_ch[4]) * int(user_days):.2f}$ ')
+        sql = f"UPDATE Cars SET Status = 2 WHERE ID = {user_ch[0]};"
         db_exec('europcar.db', sql)
 
 
